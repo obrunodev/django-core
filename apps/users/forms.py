a79efb98dee68987c56fede8c62b284622c0b4d4
Bruno R. Pianca <brunorpdev@gmail.com>
2024-08-27 00:29:10 -0300

    feat: Cadastro de usuários

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,5 +1,27 @@
-from django.contrib.auth.models import User
 from django import forms
+from apps.users.models import User
+
+
+class UserRegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username']
+    
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('As senhas não batem.')
+        return cd['password2']
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
 
 
 class UserUpdateForm(forms.ModelForm):
