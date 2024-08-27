f91d50d686012cb106d5ba5e2a921870f32f8a0f
Bruno R. Pianca <brunorpdev@gmail.com>
2024-08-27 01:07:50 -0300

    feat: Adição de alerts e tarefas por usuário (FK)

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,8 +1,10 @@
+from apps.users.models import User
 from core.models import BaseModel
 from django.db import models
 
 
 class Task(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField()
     completed = models.BooleanField(default=False)
--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,5 +1,8 @@
 from apps.tasks.forms import TaskForm
 from apps.tasks.models import Task
+from apps.tasks.mixins import UserIsOwnerMixin
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -9,23 +12,47 @@ class TaskListView(ListView):
     template_name = 'tasks/list.html'
     context_object_name = 'tasks'
 
-class TaskDetailView(DetailView):
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+
+class TaskDetailView(UserIsOwnerMixin, DetailView):
     model = Task
     template_name = 'tasks/detail.html'
 
-class TaskCreateView(CreateView):
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
     form_class = TaskForm
     template_name = 'tasks/create.html'
     success_url = reverse_lazy('tasks:list')
 
-class TaskUpdateView(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        response = super().form_valid(form)
+        messages.success(self.request, 'Task created successfully.')
+        return response
+
+
+class TaskUpdateView(UserIsOwnerMixin, UpdateView):
     model = Task
     form_class = TaskForm
     template_name = 'tasks/update.html'
     success_url = reverse_lazy('tasks:list')
 
-class TaskDeleteView(DeleteView):
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Task updated successfully.')
+        return response
+
+
+class TaskDeleteView(UserIsOwnerMixin, DeleteView):
     model = Task
     template_name = 'tasks/delete.html'
     success_url = reverse_lazy('tasks:list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, 'Task deleted successfully.')
+        return response
+    
